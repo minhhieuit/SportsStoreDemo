1753aeab602c8ccf9dc25b42eafa8556f97e1711
Cu Tí <cuti.it@hotmail.com>
2019-03-17 23:54:10 +0700

    Update latest

---
+++ SportsStore/SportsStore/Models/EFProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext dbContext;
+
+        public EFProductRepository(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+
+        public IQueryable<Product> Products => dbContext.Products;
+    }
+}
